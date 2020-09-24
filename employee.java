21e947ab176871d4802b27c23267b5e554c5477a
PrajwalRao29 <raoprajrao@gmail.com>
2020-09-24 10:39:54 +0530

    UC6

--- employee.java
+++ employee.java
@@ -3,15 +3,24 @@ public class employee {
 public static void main (String args[])
 {
 int wage=0;
-int randomValue=(int)Math.floor(Math.random()*10 % 3);
-switch(randomValue)
+int hours=0;
+int days=0;
+while(days<21 || hours<101)
 {
-case 1:wage=20*8;
-break;
-case 2:wage=20*4;
-break;
-default: wage=0;
+	int randomValue=(int)Math.floor(Math.random()*10 % 3);
+	switch(randomValue)
+	{
+	case 1:wage=wage+20*8;
+	hours=hours+8;
+	days=days+1;
+	break;
+	case 2:wage=wage+20*4;
+	hours=hours+4;
+	days=days+1;
+	break;
+	default: wage=wage+0;
+	}
 }
-System.out.println("Wage per month = "+wage*20);
+System.out.println("Wage per month = "+wage);
 }
 }
