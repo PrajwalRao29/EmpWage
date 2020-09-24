209fa1b14e1784548d01809f8be93614a1e0ca66
PrajwalRao29 <raoprajrao@gmail.com>
2020-09-24 10:24:20 +0530

    UC2

--- employee.java
+++ employee.java
@@ -2,9 +2,12 @@ package empwage;
 public class employee {
 public static void main (String args[])
 {
-double randomValue=Math.floor(Math.random()*10%2);
-System.out.println("randomValue: "+randomValue);
-String message = (randomValue==1.0)?"Employee present":"Employee absent";
-System.out.println(message);
+int wage=0;
+double randomValue=Math.floor(Math.random()*10 % 2);
+if(randomValue==1)
+{
+	wage=20*8;
+}
+System.out.println(wage);
 }
 }
