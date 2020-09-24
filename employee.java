898cb333317774db31cf81ecb88112f9858736cc
PrajwalRao29 <raoprajrao@gmail.com>
2020-09-24 10:26:37 +0530

    UC3

--- employee.java
+++ employee.java
@@ -3,10 +3,14 @@ public class employee {
 public static void main (String args[])
 {
 int wage=0;
-double randomValue=Math.floor(Math.random()*10 % 2);
+double randomValue=Math.floor(Math.random()*10 % 3);
 if(randomValue==1)
 {
 	wage=20*8;
+}
+if(randomValue==2)
+{
+	wage=20*4;
 }
 System.out.println(wage);
 }
