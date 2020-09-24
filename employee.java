5399cf270dd83d782a4eecc78832acf16ec268d6
PrajwalRao29 <raoprajrao@gmail.com>
2020-09-24 10:12:37 +0530

    UC1

--- employee.java
+++ employee.java
@@ -2,6 +2,9 @@ package empwage;
 public class employee {
 public static void main (String args[])
 {
-System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+double randomValue=Math.floor(Math.random()*10%2);
+System.out.println("randomValue: "+randomValue);
+String message = (randomValue==1.0)?"Employee present":"Employee absent";
+System.out.println(message);
 }
 }
